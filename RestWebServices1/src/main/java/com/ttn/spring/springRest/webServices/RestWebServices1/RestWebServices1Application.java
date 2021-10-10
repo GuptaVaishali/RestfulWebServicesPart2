f4d9dccb1caa31e4cfc11d46022bf807b770f751
GuptaVaishali <vaishaligupta982@gmail.com>
2021-10-11 01:58:31 +0530

    webservices1 extended to webservices2

--- RestWebServices1/src/main/java/com/ttn/spring/springRest/webServices/RestWebServices1/RestWebServices1Application.java
+++ RestWebServices1/src/main/java/com/ttn/spring/springRest/webServices/RestWebServices1/RestWebServices1Application.java
@@ -2,6 +2,11 @@ package com.ttn.spring.springRest.webServices.RestWebServices1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestWebServices1Application {
@@ -10,4 +15,10 @@ public class RestWebServices1Application {
 		SpringApplication.run(RestWebServices1Application.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver(){
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
 }
--- RestWebServices1/src/main/java/com/ttn/spring/springRest/webServices/RestWebServices1/springWelcome/WelcomeToSpringController.java
+++ RestWebServices1/src/main/java/com/ttn/spring/springRest/webServices/RestWebServices1/springWelcome/WelcomeToSpringController.java
@@ -1,11 +1,18 @@
 package com.ttn.spring.springRest.webServices.RestWebServices1.springWelcome;
 
 import com.ttn.spring.springRest.webServices.RestWebServices1.springWelcome.WelcomeToSpring;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeToSpringController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/welcome-spring")
     public String SpringWorld(){
@@ -16,4 +23,11 @@ public class WelcomeToSpringController {
     public WelcomeToSpring springBootWorld(){
         return new WelcomeToSpring("Welcome to spring boot");
     }
+
+    @GetMapping(path = "/hello-employee-itn")
+    public String helloWorldInternationalized(@RequestParam String username){
+        return messageSource.getMessage("good.morning.message",null,
+                LocaleContextHolder.getLocale()) + " " + username;
+    }
+
 }
