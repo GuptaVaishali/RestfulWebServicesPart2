4f22a180b6b6f1df68f04b711b0f387d5891fd03
GuptaVaishali <vaishaligupta982@gmail.com>
2021-10-10 18:38:57 +0530

    project set up

---
+++ RestWebServices1/src/main/java/com/ttn/spring/springRest/webServices/RestWebServices1/RestWebServices1Application.java
@@ -0,0 +1,13 @@
+package com.ttn.spring.springRest.webServices.RestWebServices1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebServices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWebServices1Application.class, args);
+	}
+
+}
---
+++ RestWebServices1/src/test/java/com/ttn/spring/springRest/webServices/RestWebServices1/RestWebServices1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn.spring.springRest.webServices.RestWebServices1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebServices1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
